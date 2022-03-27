ff8b0b448922a636ef57167562a3952f390f22f7
Bishnu <bishnuthapako@gmail.com>
2022-03-27 10:02:03 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = () => {
+    return 2000/100;
+
+}
+const square = (x) => {
+    return x*x;
+}
+console.log(square())
+
+const add = (a,b) => {
+    return a+b;
+
+};
+console.log(add(2,5))
